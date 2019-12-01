f0389a7d3b4ada3eaf1d9ed919bf5ad5adc6b264
Thien Chi Vi <meomeocf98@gmail.com>
2019-12-01 17:49:53 +0700

    Complete favorite ui (#31)

--- lib/favorite/favorite.dart
+++ lib/favorite/favorite.dart
@@ -1,5 +1,11 @@
 library tnews.favorite;
 
+import 'package:ddi/di.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tnews/common/widgets/widgets.dart';
+import 'package:tnews/news_feed/widget.dart/widget.dart';
+import 'package:tnews_core/domain/domain.dart';
+import 'package:tnews_core/tnews_core.dart';
 
 part 'favorite_screen.dart';
