67cc1f7b7695325e6ab3d92f1f261e29bf444f5d
Thien Chi Vi <meomeocf98@gmail.com>
2019-12-02 21:17:42 +0700

    Complete ontap news (#39)

--- lib/history/history.dart
+++ lib/history/history.dart
@@ -3,6 +3,8 @@ library tnews.history;
 import 'package:ddi/ddi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tnews/common/widgets/widgets.dart';
+import 'package:tnews/news_detail/news_detail.dart';
 import 'package:tnews/news_feed/widget.dart/widget.dart';
 import 'package:tnews_core/domain/domain.dart';
 import 'package:tnews_core/tnews_core.dart';
